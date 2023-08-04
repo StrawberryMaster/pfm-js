c883a2f51805ccb9ba9d2f02da5b4aaef029e60b
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2023-08-04 15:00:23 +0000

    Added owner check

--- commands/utility/shutdown.js
+++ commands/utility/shutdown.js
@@ -1,10 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { ownerId } = require('../../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('shutdown')
 		.setDescription('Shuts down the bot.'),
 	async execute(interaction) {
+		if (interaction.user.id !== ownerId) {
+			return await interaction.reply('You are not authorized to use this command, bro. :angry:');
+		}
+
 		await interaction.reply('Shutting down... :compression:');
 		process.exit();
 	},
