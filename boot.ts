b9dc0d5262b543a154274690ce0198677f2c8774
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2023-09-10 17:14:37 +0000

    Preparations for moving to Bun + general updates

---
+++ boot.ts
@@ -0,0 +1,26 @@
+import { spawn } from 'child_process';
+import fs from 'fs';
+
+function runScript(scriptPath: string, outputPath: string, scriptName: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const process = spawn('node', [scriptPath]);
+    const outStream = fs.createWriteStream(outputPath, { flags: 'a' });
+
+    process.stdout?.pipe(outStream);
+    process.stderr?.pipe(outStream);
+
+    console.log(`${scriptName} started successfully.`);
+
+    process.on('close', (code) => {
+      if (code !== 0) {
+        return reject(new Error(`[JINKIES] Process exited with code ${code}.`));
+      }
+      resolve();
+    });
+  });
+}
+
+Promise.all([
+  runScript('./deploy-commands.js', './deploy-commands.log', 'deploy-commands.js'),
+	runScript('./index.js', './index.log', 'index.js'),
+]).catch((error) => console.error(`Error: ${error.message}`));
