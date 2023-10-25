960dfdab4b40d09d291acbff8ccf2370ee26a6f2
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2023-10-25 15:59:34 +0000

    Refactored boot.ts

--- boot.ts
+++ boot.ts
@@ -1,28 +1,29 @@
 import { spawn } from 'child_process';
 import fs, { WriteStream } from 'fs';
 
-function runScript(scriptPath: string, outputPath: string, scriptName: string): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const process = spawn('bun', [scriptPath]);
-    const outStream: WriteStream = fs.createWriteStream(outputPath, { flags: 'a' });
+async function runScript(scriptPath: string, outputPath: string, scriptName: string): Promise<void> {
+  const process = spawn('bun', [scriptPath]);
+  const outStream = fs.createWriteStream(outputPath, { flags: 'a' });
 
-    process.stdout.on('data', (data) => {
-      const now = new Date().toISOString();
-      outStream.write(`[${now}] [${scriptName}] ${data}`);
-    });
+  for await (const data of process.stdout) {
+    const now = new Date().toISOString();
+    outStream.write(`[${now}] [${scriptName}] ${data}`);
+  }
 
-    process.stderr.on('data', (data) => {
-      const now = new Date().toISOString();
-      outStream.write(`[${now}] [${scriptName}] ${data}`);
-    });
+  for await (const data of process.stderr) {
+    const now = new Date().toISOString();
+    outStream.write(`[${now}] [${scriptName}] ${data}`);
+  }
 
-    console.log(`${scriptName} started successfully.`);
+  console.log(`${scriptName} started successfully.`);
 
+  await new Promise<void>((resolve, reject) => {
     process.on('close', (code) => {
       if (code !== 0) {
-        return reject(new Error(`[JINKIES] Process exited with code ${code}.`));
+        reject(new Error(`[JINKIES] Process exited with code ${code}.`));
+      } else {
+        resolve();
       }
-      resolve();
     });
   });
 }
