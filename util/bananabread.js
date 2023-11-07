06a1c14c252d4ef2b74202438eb05b90eb762107
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2023-11-07 23:21:19 +0000

    Add randomElement and randomHex to bananabread

--- util/bananabread.js
+++ util/bananabread.js
@@ -4,4 +4,10 @@ module.exports = {
 	randomColor : () => {
 		return Math.floor(Math.random() * 16777215);
 	},
+	randomElement : (array) => {
+		return array[Math.floor(Math.random() * array.length)];
+	},
+	randomHex : () => {
+		return '#' + Math.floor(Math.random() * 16777215).toString(16);
+	},
 };
