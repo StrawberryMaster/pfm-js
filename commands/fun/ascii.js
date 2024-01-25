5bf18b5977f3bd1bd31689764e7892233c4547b2
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2024-01-25 19:28:19 +0000

    Added ascii.js

---
+++ commands/fun/ascii.js
@@ -0,0 +1,31 @@
+// Attribution goes to https://github.com/skillzl/eres/blob/main/commands/utils/whois.js for the original code
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { randomColor } = require('../../util/bananabread.js');
+const figlet = require('figlet');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ascii')
+		.setDescription('Converts text to ASCII art')
+		.addStringOption(option => option.setName('text').setDescription('The text to convert').setRequired(true)),
+	async execute(interaction) {
+		const text = interaction.options.getString('text');
+
+		figlet(text, async (error, artData) => {
+			if (error) {
+				console.log('Huh. Something went wrong.');
+				console.dir(error);
+				return;
+			}
+
+			const embed = new EmbedBuilder()
+				.setColor(randomColor())
+				.setTitle('Your ASCII art')
+				.setDescription('```' + artData + '```')
+				.setTimestamp()
+				.setFooter({ text: 'Use /fortune to get a new quote.', iconURL: interaction.user.avatarURL() });
+
+			await interaction.reply({ embeds: [embed] });
+		});
+	},
+};
