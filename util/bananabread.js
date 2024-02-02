97f020c14cacfd1dfa4b4215c6a7c06d9005a6c4
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2024-02-02 19:36:03 +0000

    Cleanup

--- util/bananabread.js
+++ util/bananabread.js
@@ -17,10 +17,4 @@ module.exports = {
 	randomColor : () => {
 		return Math.floor(Math.random() * 16777215);
 	},
-	randomElement : (array) => {
-		return array[Math.floor(Math.random() * array.length)];
-	},
-	randomHex : () => {
-		return '#' + Math.floor(Math.random() * 16777215).toString(16);
-	},
 };
