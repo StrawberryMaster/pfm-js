ffc2404e9b251ca31084509dba8f95044d30414c
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2024-03-24 19:48:38 +0000

    Moved quotes to a separate file

---
+++ data/fortunequotes.js
@@ -0,0 +1,17 @@
+// for fortune.js
+module.exports = [
+	{ quote: 'First thing that\'s different, no more Dad. He threatened to turn me into the government so I made him and the government go away.', author: 'Rick Sanchez' },
+	{ quote: 'My friend\'s name was Megatron and he had [...] three things you should demand to know of any powerful institution. [...] In whose interest do you exercise power? [...] To whom are you accountable? [...] How can we get rid of you?', author: 'Optimus Prime' },
+	{ quote: 'Our enemies are innovative and resourceful, and so are we. They never stop thinking about new ways to harm our country and our people, and neither do we.', author: 'George W. Bush' },
+	{ quote: 'In \'The Lion, the Witch and the Wardrobe\', a bunch of hippies walk around and paint stuff. They eat lunch, and then they find a magical camel, which they have to eat to stay alive. And that\'s pretty much it. I give it a B-minus.', author: 'Eric Cartman' },
+	{ quote: 'I want death to find me planting my cabbages.', author: 'Michel de Montaigne' },
+	{ quote: 'Maybe I should just kill Mike. I\'m the chief of police, I could cover it up.', author: 'Jim Hopper' },
+	{ quote: 'See, baby, I got everything... Mink sheets... Mink coats... Mink curtains in the window. When I walk down the stairs, I\'m walking down on... mink carpet.', author: 'Jizzy B' },
+	{ quote: 'Nothing drives down real estate prices like a good old-fashioned gang war. Apart from an outbreak of plague, but that may be going too far in this case.', author: 'Donald Love' },
+	{ quote: 'I always look orange. And so do you. The light is the worst.', author: 'Donald Trump' },
+	{ quote: 'If you think nothing can get to you, you\'re lying to yourself. At best you\'re temporarily dead. A lightning bolt could re-animate you without a warning.', author: 'Max Payne' },
+	{ quote: 'I was thinking about why so many in the radical left participate in \'speedrunning\'. The reason is the left\'s lack of work ethic... (\'go fast\' rather than \'do it right\') and, in a Petersonian sense... to elevate alternative sexual archetypes in the marketplace (\'fastest hedgehog\')... you\'re a beta male, Sonic.', author: 'Shadow the Hedgehog' },
+	{ quote: 'Welcome to Good Burger, home of the Good Burger. Can I take your order?', author: 'Ed, \'Good Burger\'' },
+	{ quote: 'I\'m saving my earnings for an electron microscope! I\'ve only got nineteen thousand, four hundred and seventy-two dollars and eighteen cents to go!', author: 'Edd' },
+	{ quote: 'Behold, the Melt-inator 6-5000! It has a melting capacity... of 7! That\'s on a scale from 1 to 5, so that\'s a big number.', author: 'Dr. Heinz Doofenshmirtz' },
+];
