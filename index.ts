711dadc334c722128d109b53f8e461fa57556a2b
Felipe <41008398+StrawberryMaster@users.noreply.github.com>
2024-09-11 20:29:22 +0000

    Updated dependencies

--- index.ts
+++ index.ts
@@ -20,19 +20,19 @@ const commandFolders = fs.readdirSync(foldersPath);
 
 // Loop over all the files in the commands folder and set them as commands
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.js') || file.endsWith('.ts'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        }
-        else {
-			console.error(`Zoinks: a command @ ${filePath} is missing a required data or execute property.`);
-		}
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.js') || file.endsWith('.ts'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
     }
+    else {
+      console.error(`Zoinks: a command @ ${filePath} is missing a required data or execute property.`);
+    }
+  }
 }
 
 // Set up the events
